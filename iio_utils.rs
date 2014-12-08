df9947ad885b9ddae1ce16d298efba7750c2710f
ezra <ezra@indico.io>
2014-12-08 15:24:03 -0500

    initial commit

---
+++ iio_utils.rs
@@ -0,0 +1,11 @@
+extern crate libc;
+use libc::{c_char, c_int};
+
+#[link(name = "libiio")]
+extern {
+    fn set_dev_paths(device_name: *mut c_char) -> c_int;
+}
+
+fn main(){
+    unsafe {println!("your dir {}", set_dev_paths(*mut "ad9361-phy") as i16)}
+}
